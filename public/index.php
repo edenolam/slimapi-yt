a1658e037c54c7d6f8ed6b52efba3463bf0baf23
julien basquin <julienbasquin.jb@gmail.com>
2021-08-04 13:28:43 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config/db.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// friends routes
+require __DIR__ . '/../routes/friends.php';
+
+$app->run();
